21fa24ed8fbe65ea8e5cf6ac5170a869d6e4e656
Natalia Anjos <naty_anjos2@hotmail.com>
2020-08-09 15:29:31 -0300

    titulo

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Bem vindo a <a href="https://nextjs.org">casa da Natalia</a>
         </h1>
 
         <p className={styles.description}>
